2b78cef6927b4cdb914d7b71df8ea0347004046d
Onyibe Godstime <godstimeonyibe2@gmail.com>
2020-09-30 13:35:00 +0100

    module three completed

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import { Album } from './album';
 @Injectable()
 export class ProductService {
-  private  _albumUrl = '../assets/album.json';
-  constructor( private _http: Http) {}
+  private _albumUrl = '../assets/album.json';
+  private  _productsUrl  = '../assets/products.json';
+  constructor( private _http: Http) {
+
+  }
    getAlbum(id: number){
      return this._http.get(this._albumUrl)
-     .map((response) => {
-        response.json()
-     });
+     .map((response) => <Album>response.json());
    }
 }
